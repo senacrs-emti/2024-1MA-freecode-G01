82e48b7738d801e81b8f02f38fcc117d13c5a02f
Maria Eduarda Strassburger Schenkel <meduarda.schenkek@gmail.com>
2024-11-21 08:19:28 -0300

    main

---
+++ experimenteM.js
@@ -0,0 +1,61 @@
+let mediaDevices = navigator.mediaDevices;
+
+document.addEventListener("DOMContentLoaded", () => {
+    let but = document.getElementById("but");
+    let video = document.getElementById("vid");
+    let mediaDevices = navigator.mediaDevices;
+    vid.muted = true;
+    but.addEventListener("click", () => {
+
+// Accessing the user camera and video.
+mediaDevices
+    .getUserMedia({
+        video: true,
+        audio: true,
+})
+.then((stream) => {
+              // Changing the source of video to current stream.
+              video.srcObject = stream;
+              video.addEventListener("loadedmetadata", () => {
+                  video.play();
+              });
+          })
+          .catch(alert);
+  });
+});
+
+// valor constante do pixel para centimetro
+const pixelCM = 0.0264583333;
+
+
+const btCalculaMedida = document.getElementById('calcula');
+btCalculaMedida.addEventListener('click', () => {
+    // captura a medida
+    let medida = document.getElementById('medida').value;
+    
+    let conversao = ((pixelCM*medida)*100)*2;
+    let roupa = document.getElementById("roupa");
+    roupa.setAttribute("width", conversao);
+
+    let manequim = document.getElementById("manequim");
+
+    if (medida <= 48){
+        tamanho = "PP"; 
+        manequim.setAttribute("src","mascP.png")
+    } else if ( medida >= 49 && medida <= 51){
+        tamanho = "P";
+        manequim.setAttribute("src","mascP.png");
+    } else if (medida >=52 && medida <= 53){
+        tamanho = "M";
+        manequim.setAttribute("src","mascM.png");
+    } else if (medida>=54 && medida <= 56){
+        tamanho = "G";
+        manequim.setAttribute("src","mascG.png");
+    } else {
+        tamanho = "GG";
+    }
+    
+    document.getElementById("tamanhos").innerHTML = "Seu tamanho é " + tamanho;
+    
+});
+
--- teste.js
+++ teste.js
@@ -1,29 +1,34 @@
-// valor constante do pixel para centimetro
-const pixelCM = 0.0264583333;
-
-
-const btCalculaMedida = document.getElementById('calcula');
-btCalculaMedida.addEventListener('click', () => {
-    // captura a medida
-    let medida = document.getElementById('medida').value;
-    
-    let conversao = ((pixelCM*medida)*100)*2;
-    let roupa = document.getElementById("roupa");
-    roupa.setAttribute("width", conversao);
-
-
-    if (medida <= 48){
-        tamanho = "PP"; 
-    } else if ( medida >= 49 && medida <= 51){
-        tamanho = "P";
-    } else if (medida >=52 && medida <= 53){
-        tamanho = "M";
-    } else if (medida>=54 && medida <= 56){
-        tamanho = "G";
-    } else {
-        tamanho = "GG";
-    }
-    
-    document.getElementById("tamanhos").innerHTML = "Seu tamanho é " + tamanho;
-    
+/*
+  This is your site JavaScript code - you can add interactivity!
+*/
+ 
+// Print a message in the browser's dev tools console each time the page loads
+// Use your menus or right-click / control-click and choose "Inspect" > "Console"
+console.log("Hello 🌎");
+ 
+/*
+Make the "Click me!" button move when the visitor clicks it:
+- First add the button to the page by following the steps in the TODO 🚧
+*/
+const btn = document.querySelector("button"); // Get the button from the page
+if (btn) { // Detect clicks on the button
+  btn.onclick = function () {
+    // The 'dipped' class in style.css changes the appearance on click
+    btn.classList.toggle("dipped");
+  };
+}
+ 
+ 
+// ----- GLITCH STARTER PROJECT HELPER CODE -----
+ 
+// Open file when the link in the preview is clicked
+let goto = (file, line) => {
+  window.parent.postMessage(
+    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
+  );
+};
+// Get the file opening button from its class name
+const filer = document.querySelectorAll(".fileopener");
+filer.forEach((f) => {
+  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
 });
